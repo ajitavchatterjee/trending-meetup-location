327d984d914a30d80996c8f6be49ba55895be7c6
Ajitav Chatterjee <cajitav@yahoo.in>
2020-07-21 02:02:10 +0200

    Clean up and fix map coordinates array update

--- meetup-map/src/app/app.component.ts
+++ meetup-map/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, ElementRef, ViewChild, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MeetupFrequencyService } from './services/meetup-frequency.service';
 import { VenueFrequency } from './model/VenueFrequency';
 import { Subscription, interval } from 'rxjs';
 import { VenueCoordItem } from './model/VenueCoordItem';
 
-declare var google: any;
+// declare var google: any;
 
 @Component({
   selector: 'app-root',
@@ -13,85 +13,70 @@ declare var google: any;
 })
 export class AppComponent implements OnDestroy {
   constructor(private sseService: MeetupFrequencyService) {}
-  lat = 51.678418;
-  lng = 7.809007;
+  lat: number = 37.774546;
+  lng: number = -122.433523;
   private map: google.maps.Map;
   private heatmap: any;
   private coords = [];
   mySub: Subscription;
-  private venueCoord = new Map();
+  private venueCoordMap = new Map();
 
   onMapLoad(mapInstance: google.maps.Map) {
     this.map = mapInstance;
 
     this.heatmap = new google.maps.visualization.HeatmapLayer({
       map: this.map,
-      data: this.coords
-  });
+      data: this.coords,
+      radius: 15
+    });
 
-  this.sseService.getMeetUpData( this.onMessageSuccess.bind(this), () => {
-    //handle the error here
-  })
+    this.sseService.getMeetUpData (this.onMessageSuccess.bind(this));
 
-
-    // call update map to array
     this.mySub = interval(30000).subscribe((func => {
-      // this.updateCoordArray(this.venueCoord);
+      this.updateCoordArray(this.venueCoordMap);
       if(this.coords.length > 0) {
         this.heatmap.setData(this.coords);
       }
     }))
-
-    // here our in other method after you get the coords; but make sure map is loaded
-    // this.sseService.getServerSentEvent().subscribe((message) => {
-    //   const obj = JSON.parse(message.data);
-    //   console.log(obj);
-    //   this.initHeatMap(obj.venueFrequencyKey.lat,obj.venueFrequencyKey.lon, obj.count, obj.venueFrequencyKey.venue_id)
-    // });
 }
 
 onMessageSuccess(data: MessageEvent['data']) {
-  const obj : VenueFrequency = JSON.parse(data);
-  console.log(obj);
-  const temp = [...this.coords];
-  const index = temp.findIndex( t => t.venue_id === obj.venueFrequencyKey.venue_id)
-  if(index > -1) {
-    temp[index] = {location: this.formatLatLng(obj.venueFrequencyKey.lat, obj.venueFrequencyKey.lon), weight: obj.count};
-  } else {
-    temp.push({location: this.formatLatLng(obj.venueFrequencyKey.lat, obj.venueFrequencyKey.lon), weight: obj.count})
-  }
-  this.coords = temp;
-  // this.coords = obj.map(t => ({location: (this.formatLatLng(t.venueFrequencyKey.lat, t.venueFrequencyKey.lon)), weight: t.count}))
-  // this.heatmap.setData(this.coords);
-  // this.updateVenueMap(obj.venueFrequencyKey.lat,obj.venueFrequencyKey.lon, obj.count, obj.venueFrequencyKey.venue_id)
-  // this.coords.push({location: (this.formatLatLng(value.lat, value.lon)), weight: value.weight})
+  const venueFrequency : VenueFrequency = JSON.parse(data);
+  this.updateVenueCoordMap(venueFrequency);
 }
 
-onMessageError() {
-
+updateVenueCoordMap(venueFrequency: VenueFrequency) {
+  this.venueCoordMap.set(
+    venueFrequency.venueFrequencyKey.venue_id, 
+    new VenueCoordItem(
+      venueFrequency.venueFrequencyKey.lat, 
+      venueFrequency.venueFrequencyKey.lon, 
+      venueFrequency.count
+    )
+  );
 }
 
-  // initHeatMap(lat: number, lon: number, weight: number, venue_id: number) {
-  //   this.updateVenueMap(lat, lon, weight, venue_id);
-  // }
+updateCoordArray(venueCoordMap: Map<number,VenueCoordItem>) {
+  this.coords.splice(0,this.coords.length);
+  venueCoordMap.forEach((value: VenueCoordItem, _) => {
+    this.coords.push({location: (this.formatLatLng(value.lat, value.lon)), weight: value.weight})
+});
+}
 
-  updateVenueMap(lat: number, lon: number, weight: number, venue_id: number) {
-    this.venueCoord.set(venue_id, new VenueCoordItem(lat, lon, weight));
-    debugger;
-  }
+formatToHeatMapData(venueFrequency: VenueFrequency) {
+  return {
+    location: this.formatLatLng(
+      venueFrequency.venueFrequencyKey.lat, 
+      venueFrequency.venueFrequencyKey.lon), 
+    weight: venueFrequency.count
+  };
+}
 
-  formatLatLng(lat: number, lon: number) {
-    return new google.maps.LatLng(lat, lon);
-  }
+formatLatLng(lat: number, lon: number) {
+  return new google.maps.LatLng(lat, lon);
+}
 
-  updateCoordArray(venueCoord: Map<number,VenueCoordItem>) {
-    this.coords.splice(0,this.coords.length);
-    for (let [key, value] of venueCoord) {
-      this.coords.push({location: (this.formatLatLng(value.lat, value.lon)), weight: value.weight})
-    }
-  }
-
-  ngOnDestroy() {
-    this.mySub.unsubscribe();
-  }
+ngOnDestroy() {
+  this.mySub.unsubscribe();
+}
 }
--- meetup-map/src/app/services/meetup-frequency.service.ts
+++ meetup-map/src/app/services/meetup-frequency.service.ts
@@ -3,22 +3,21 @@ import { Observable } from "rxjs";
 import { environment } from 'src/environments/environment';
 
 type OnMessageCallback = (data: MessageEvent['data']) => void
-type OnErrorCallback = () => void
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeetupFrequencyService {
 
-  getMeetUpData(onMessageCallback: OnMessageCallback, onErrorCallback: OnErrorCallback) {
+  getMeetUpData(onMessageCallback: OnMessageCallback) {
     let sse = new EventSource(environment.meetUpFrequencyUrl);
 
     sse.onmessage = function(e) {
       onMessageCallback(e.data);
     }
 
-    sse.onerror = function() {
-      onErrorCallback()
+    sse.onerror = function(e) {
+      console.log(e);
     }
   }
 }
