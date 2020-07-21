6f1d85935e4036377673be79c50711ef958334b8
Ajitav Chatterjee <cajitav@yahoo.in>
2020-07-21 12:06:45 +0200

    Removed unused meetup-service

--- meetup-service/src/main/java/com/solution/meetupservice/service/MeetUpService.java
+++
@@ -1,9 +0,0 @@
-package com.solution.meetupservice.service;
-
-import com.solution.meetupservice.model.VenueFrequency;
-
-import java.util.List;
-
-public interface MeetUpService {
-    List<VenueFrequency> popularVenueMeetups() ;
-}
